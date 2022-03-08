ba0bc2fe1f85cbddddcc339e06d5799ef537a7f0
iipekolict <iipekolict@gmail.com>
2022-03-08 21:55:35 +0300

    fix: 3

--- src/pages/shared/[id].tsx
+++ src/pages/shared/[id].tsx
@@ -9,8 +9,6 @@ import { StyledLink } from '../../components/styled-components';
 import { API_URL } from '../../shared/constants';
 import { join } from 'path';
 import { ParsedUrlQuery } from 'querystring';
-import { useShared } from '../../hooks/useShared';
-import Link from 'next/link';
 
 export const getServerSideProps: GetServerSideProps = async (
   context: GetServerSidePropsContext
@@ -19,28 +17,20 @@ export const getServerSideProps: GetServerSideProps = async (
   const fileNames: string[] = await (
     await fetch(join(API_URL, id || ''))
   ).json();
-  const links: string[] = fileNames.map((fileName: string) =>
-    join(API_URL, id || '', fileName)
-  );
+  // const links: string[] = fileNames.map((fileName: string) =>
+  //   join(API_URL, id || '', fileName)
+  // );
 
-  return { props: { dir: id, fileNames, links } };
+  return { props: { dir: id, fileNames } };
 };
 
-const SharedFiles: NextPage<SharedFilesPageProps> = ({
-  dir,
-  fileNames,
-  links,
-}) => {
-  const { openFile } = useShared(links);
-
+const SharedFiles: NextPage<SharedFilesPageProps> = ({ dir, fileNames }) => {
   return (
     <Layout title={dir}>
       <h1>{dir} directory</h1>
       {fileNames.map((fileName: string) => (
         <StyledLink key={fileName}>
-          <Link href={join(API_URL, dir, fileName)}>
-            <a>{fileName}</a>
-          </Link>
+          <a href={`${API_URL}/${dir}/${fileName}`}>{fileName}</a>
         </StyledLink>
       ))}
     </Layout>
